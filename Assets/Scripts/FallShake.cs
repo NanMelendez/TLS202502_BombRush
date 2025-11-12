f22c3fc7a2c54639d3018f2e331625b8e8a9d856
u201922331 <u201922331@upc.edu.pe>
2025-11-12 00:08:20 -0500

    Mapa 1 mejorado

---
+++ Assets/Scripts/FallShake.cs
@@ -0,0 +1,14 @@
+using Unity.Cinemachine;
+using UnityEngine;
+
+public class FallShake : MonoBehaviour
+{
+    public float amplitude;
+    public float frequency;
+    private Rigidbody2D rb2d;
+
+    void Update()
+    {
+        
+    }
+}
